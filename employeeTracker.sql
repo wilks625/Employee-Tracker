e307323811bbaaa987a38b1da1baa4e7aaa03c81
Stephen Yacas <syacas123@gmail.com>
2021-05-10 21:58:26 -0400

    first commit

---
+++ employeeTracker.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS trackEmployeeDB;
+CREATE database trackEmployeeDB;
+
+USE trackEmployeeDB;
+
+CREATE TABLE department (
+  ID INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NULL,
+  PRIMARY KEY (ID)
+);
+
+CREATE TABLE role (
+  ID INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NULL,
+  salary DECIMAL(10,2) NULL,
+  department_id INT NULL,
+  PRIMARY KEY (ID)
+);
+
+CREATE TABLE employee (
+  ID INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NULL,
+  last_name VARCHAR(30) NULL,
+  role_id INT NULL,
+  manager_id INT NULL,
+  PRIMARY KEY (ID)
+);
+
+SELECT * FROM department;
+SELECT * FROM role;
+SELECT * FROM employee;
---
+++ index.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: 'root',
+
+  // Be sure to update with your own MySQL password!
+  password: '',
+  database: 'top_songsDB',
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  runSearch();
+});
